1a599fbd883989e8a776708b09397f3e4e63c5d8
Yang <ybh1760@gmail.com>
2019-10-13 22:12:28 +0900

    화면 추가

---
+++ screens/FavoritesScreen.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const FavoritesScreen = props => {
+  return (
+    <View style={styles.screen}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
+
+export default FavoritesScreen;
